0d2e9f9306a6f544376682eeb0c6163edfcafc0d
BugZ-EKB <e.shorohov87@yandex.ru>
2023-08-03 22:53:20 +0400

    Solution 4

--- tasks/task.py
+++ tasks/task.py
@@ -5,20 +5,20 @@ from time import sleep
 execution_time: Dict[str, float] = {}
 
 def time_decorator(fn):
-    def wrapper(a, b, d):
+    def wrapper(*args, **kwargs):
         start = time.time()
-        fn(a,b,d)
+        fn(*args, **kwargs)
         end = time.time()
-        c = end-start + d
+        c = end-start
         execution_time[str(fn.__name__)] = c
     return wrapper
     pass
 # @time_decorator
-# def func_add(a: int, b: int):
-#     sleep(0.2)
+# def func_add(a: int, b: int, sleep_time: int):
+#     sleep(sleep_time)
 #     return a + b
 #
-# func_add(10, 20)
+# func_add(10, 20, 0.1)
 # print(execution_time)
 # print(execution_time['func_add'])
 # 0.212341254
